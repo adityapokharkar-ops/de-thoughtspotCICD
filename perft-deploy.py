6fc8a3ebf272a4a2d3d85263dd48365db2f673f7
adityapokharkar-ops <aditya.pokharkar@simpplr.com>
2026-01-21 18:42:56 +0530

    .

--- perft-deploy.py
+++ perft-deploy.py
@@ -85,7 +85,7 @@ def deploy_call(Bearer_token):
     payload = {
         "branch_name": "perft",
         "deploy_type": "DELTA",
-        "deploy_policy": "ALL_OR_NONE"
+        "deploy_policy": "PARTIAL"
     }
 
     try:
